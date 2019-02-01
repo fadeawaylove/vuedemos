4ec467e4b2f18475b82547fb118484b769371c64
邓润庭 <1032939141@qq.com>
2019-02-01 11:04:40 +0800

    add

---
+++ demos/21-路由嵌套/demo/src/components/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <ul>
+      <li v-for="(item, key) in products">
+        <router-link :to="'/pcontent?aid=' + key">{{key}}:{{item}}</router-link>
+      </li>
+    </ul>
+    <button @click="toNews()">跳转到新闻列表</button>
+    <br>
+    <button @click="toDetails()">跳转到新闻详情496</button>
+
+    <br>
+    <hr>
+    <button @click="goNews()">跳转新闻里列表（匿名跳转）</button>
+    <br>
+    <button @click="goDetails()">跳转新闻里详情（匿名跳转）</button>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: ["电视机", "冰箱", "彩电"]
+    };
+  },
+  methods:{
+    toNews(){
+      this.$router.push({path: "news"})
+    },
+    toDetails(){
+      this.$router.push({path: "content/496"})
+    },
+    // 匿名跳转
+    goNews(){
+      this.$router.push({name: "news"})
+    },
+    goDetails(){
+      this.$router.push({name: "content", params:{aid:496}})
+    }
+  }
+};
+</script>
---
+++ demos/21-路由嵌套/demo/src/components/user/UserAdd.vue
@@ -0,0 +1,6 @@
+<template>
+    <div id='useradd'>
+        <h2>添加用户</h2>
+
+    </div>
+</template>
